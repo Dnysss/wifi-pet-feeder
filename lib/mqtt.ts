78b0b0c733cb14592a711b83bde7bb433d0cb486
Dnysss <denysantos2014@hotmail.com>
2026-09-10 20:48:48 -0300

    organizando arquivo

--- lib/mqtt.ts
+++ lib/mqtt.ts
@@ -1,32 +1,20 @@
 import mqtt, { MqttClient } from "mqtt";
 
 // CONFIGURAÇÕES DO HIVEMQ
-
 const MQTT_BROKER = process.env.EXPO_PUBLIC_MQTT_BROKER!;
-
 const MQTT_USERNAME = process.env.EXPO_PUBLIC_MQTT_USERNAME!;
-
 const MQTT_PASSWORD = process.env.EXPO_PUBLIC_MQTT_PASSWORD!;
 
 // TÓPICOS
-
 export const MQTT_TOPIC_COMANDO = "denys/alimentador/comando";
-
 export const MQTT_TOPIC_STATUS = "denys/alimentador/status";
 
 // CLIENTE MQTT
-
 let client: MqttClient | null = null;
-
-// CALLBACK DE STATUS
-
 let statusCallback: ((status: string) => void) | null = null;
 
-// CONECTAR AO HIVEMQ
-
 export function conectarMQTT(): MqttClient {
-  // Se já existe uma conexão,
-  // reutiliza o cliente
+  // Se já existe uma conexão e retorna o cliente existente
   if (client) {
     return client;
   }
@@ -35,21 +23,13 @@ export function conectarMQTT(): MqttClient {
 
   client = mqtt.connect(MQTT_BROKER, {
     username: MQTT_USERNAME,
-
     password: MQTT_PASSWORD,
-
     protocol: "wss",
-
     reconnectPeriod: 5000,
-
     connectTimeout: 10000,
-
     clean: true,
-
     clientId: "Expo-Alimentador-" + Math.random().toString(16).substring(2, 10),
   });
-
-  // CONECTADO
 
   client.on("connect", () => {
     console.log("MQTT conectado ao HiveMQ!");
@@ -63,8 +43,6 @@ export function conectarMQTT(): MqttClient {
     });
   });
 
-  // RECEBER MENSAGENS
-
   client.on("message", (topic, message) => {
     const mensagem = message.toString();
 
@@ -77,19 +55,13 @@ export function conectarMQTT(): MqttClient {
     }
   });
 
-  // ERRO
-
   client.on("error", (error) => {
     console.error("Erro MQTT:", error);
   });
 
-  // DESCONECTADO
-
   client.on("close", () => {
     console.log("MQTT desconectado.");
   });
-
-  // RECONEXÃO
 
   client.on("reconnect", () => {
     console.log("Tentando reconectar ao MQTT...");
@@ -98,18 +70,14 @@ export function conectarMQTT(): MqttClient {
   return client;
 }
 
-// PUBLICAR COMANDO
-
 export function enviarComando(segundos: number): void {
   if (!client) {
     console.error("MQTT não está conectado.");
-
     return;
   }
 
   if (!client.connected) {
     console.error("MQTT ainda não está conectado.");
-
     return;
   }
 
@@ -124,8 +92,6 @@ export function enviarComando(segundos: number): void {
   });
 }
 
-// PARAR MOTOR
-
 export function pararMotor(): void {
   enviarComando(0);
 }
@@ -137,18 +103,14 @@ export function definirCallbackStatus(
   statusCallback = callback;
 }
 
-//VERIFICAR CONEXÃO
 export function estaConectado(): boolean {
   return client !== null && client.connected;
 }
 
-//DESCONECTAR
 export function desconectarMQTT(): void {
   if (client) {
     client.end();
-
     client = null;
-
     console.log("MQTT desconectado manualmente.");
   }
 }
